93ba07485aced43d98947ddc910825ef4c650eff
webinstats <>
2017-10-09 11:16:05 +0300

    some changes

--- DemoExample/app/src/main/java/webinstats/demoexample/DeepLink.java
+++ DemoExample/app/src/main/java/webinstats/demoexample/DeepLink.java
@@ -6,6 +6,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import webinstats.android_wis_141.Webinstats;
+
 /**
  * Created by fatmaselin on 22/09/2017.
  */
@@ -15,8 +17,9 @@ public class DeepLink extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.deep_link);
-
+        Webinstats wobj = new Webinstats("//wisdemo.webinstats.com/","4e84-1481","0");
         Intent appIntent = getIntent();
         Uri appLinkData = appIntent.getData();
+        wobj.saveTestParameters(this,appLinkData);
     }
 }
